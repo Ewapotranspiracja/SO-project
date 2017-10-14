64128da34e8a5e1baeb04cb7ca625aa09af433ae
masterkwi <cezaryczekalski@gmail.com>
2017-10-14 21:26:57 +0200

    masterkwi2

--- src/so_projekt/Interfejs_graficzny.java
+++ src/so_projekt/Interfejs_graficzny.java
@@ -33,7 +33,7 @@ public class Interfejs_graficzny extends JFrame {
             }
         });
 
-        JButton Modul2 = new JButton("zarzadzeni procesorem");
+        JButton Modul2 = new JButton("zarzadzenie procesorem");
         Modul2.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
